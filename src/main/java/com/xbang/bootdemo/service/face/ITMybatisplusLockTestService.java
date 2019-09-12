783a2dfa9d68cb29de4f4ec461cd10bd441f1a8d
1523550440@qq.com <1523550440@qq.com>
2019-09-12 15:05:57 +0800

    --mbbatis 乐观锁插件测试

---
+++ src/main/java/com/xbang/bootdemo/service/face/ITMybatisplusLockTestService.java
@@ -0,0 +1,16 @@
+package com.xbang.bootdemo.service.face;
+
+import com.xbang.bootdemo.dao.entity.TMybatisplusLockTest;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author xbang
+ * @since 2019-09-12
+ */
+public interface ITMybatisplusLockTestService extends IService<TMybatisplusLockTest> {
+
+}
---
+++ src/main/java/com/xbang/bootdemo/service/impl/TMybatisplusLockTestServiceImpl.java
@@ -0,0 +1,20 @@
+package com.xbang.bootdemo.service.impl;
+
+import com.xbang.bootdemo.dao.entity.TMybatisplusLockTest;
+import com.xbang.bootdemo.dao.mapper.TMybatisplusLockTestMapper;
+import com.xbang.bootdemo.service.face.ITMybatisplusLockTestService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author xbang
+ * @since 2019-09-12
+ */
+@Service
+public class TMybatisplusLockTestServiceImpl extends ServiceImpl<TMybatisplusLockTestMapper, TMybatisplusLockTest> implements ITMybatisplusLockTestService {
+
+}
